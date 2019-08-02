51dee9e5f49827fc76b2f4e327c76617d0521ba1
Kuldeep Pisda <pisdak79@gmail.com>
2019-08-03 02:42:17 +0530

    Merge: merged dev

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import React from 'react'
-import { AppRegistry } from 'react-native-web'
+import Document, { Head, Main, NextScript } from "next/document";
+import React from "react";
+import { AppRegistry } from "react-native-web";
 
 // Force Next-generated DOM elements to fill their parent's height
 const normalizeNextElements = `
@@ -9,31 +9,54 @@ const normalizeNextElements = `
     flex-direction: column;
     height: 100%;
   }
-`
+`;
 
 export default class MyDocument extends Document {
-  static async getInitialProps ({ renderPage }) {
-    AppRegistry.registerComponent('Main', () => Main)
-    const { getStyleElement } = AppRegistry.getApplication('Main')
-    const page = renderPage()
+  static async getInitialProps({ renderPage }) {
+    AppRegistry.registerComponent("Main", () => Main);
+    const { getStyleElement } = AppRegistry.getApplication("Main");
+    const page = renderPage();
     const styles = [
       <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
       getStyleElement()
-    ]
-    return { ...page, styles: React.Children.toArray(styles) }
+    ];
+    return { ...page, styles: React.Children.toArray(styles) };
   }
 
-  render () {
+  render() {
     return (
-      <html style={{ height: '100%' }}>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-        </Head>
-        <body style={{ height: '100%', overflow: 'hidden' }}>
+      <html style={{ height: "100%" }}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+          />
+          <title>ScientificJournal</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="./static/css/styles.min.css?h=48930a4107b1ca5eb39ae1e606c0ea3f"
+          />
+          <link rel="stylesheet" href="./static/css/index.css" />
+          <link rel="stylesheet" href="./static/bootstrap/css/bootstrap.min.css?h=8a6c2afcccaaabb2f6a787d71efc0bd9" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" />
+        </head>
+        <body>
           <Main />
           <NextScript />
         </body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" />
       </html>
-    )
+    );
   }
 }

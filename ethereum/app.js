38aeb5c198c2293693f676419446733a790c57c7
Shritesh <shritesh.sj@gmail.com>
2019-08-03 16:35:04 +0530

    updated app.js file

--- ethereum/app.js
+++ ethereum/app.js
@@ -17,5 +17,12 @@ var review = new ethers.Contract(reviewContractAddress, ReviewABI, wallet);
 var store = new ethers.Contract(storeContractAddress, StoreABI, wallet);
 
 module.exports = {
-  createUser: async email => review.createUser(email),
+  createUser: async (email) => review.createUser(email).then(r => r).catch(e => console.log(e)),
+  getUser: async () => review.getUser().then(r => r).catch(e => console.log(e)),
+  updateUser: async (email) => review.updateUser(email).then(r => r).catch(e => console.log(e)),
+  deleteUser: async () => review.deleteUser().then(r => r).catch(e => console.log(e)),
+  addFile: async (hash, email, rating) => review.addFile(hash, email, rating).then(r => r).catch(e => console.log(e)),
+  getUserFileLength: async () => review.getUserFileLength().then(r => r).catch(e => console.log(e)),
+  getUserFilebyIndex: async (index) => review.getUserFilebyIndex(index).then(r => r).catch(e => console.log(e)),
+  updateUserFilebyIndex: async (index, name, rating) => review.updateUserFilebyIndex(index, name, rating).then(r => r).catch(e => console.log(e))
 };

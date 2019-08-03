a86206f0d83ced466743d45e6e64fbf7b740fb9d
Kuldeep Pisda <pisdak79@gmail.com>
2019-08-04 00:25:56 +0530

    wip: register

--- pages/register.js
+++ pages/register.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { inject } from "mobx-react";
+import Router from "next/router";
+import { runInAction } from "mobx";
+import { review } from "../ethereum/app";
 
+@inject("mobx")
 export default class Register extends React.Component {
-  componentDidMount(){
+  state = {
+    name: "",
+    ethAddress: ""
+  };
+  componentDidMount() {
     document.body.classList.add("bg-gradient-primary");
   }
 
-  componentDidUnmount(){
+  componentDidUnmount() {
     document.body.classList.remove("bg-gradient-primary");
   }
-  
+
   render() {
+    const { user } = this.props.store;
+    if (user.address !== "guesttoken") Router.push("/dashboard");
     return (
       <div className="container">
         <div className="row">
@@ -24,18 +35,70 @@ export default class Register extends React.Component {
                   <br />
 
                   <form className="user">
+                    <div className="form-group">
+                      <input
+                        className="form-control form-control-user"
+                        type="text"
+                        id="name"
+                        aria-describedby="nameHelp"
+                        placeholder="Enter Your Name"
+                        name="name"
+                        onChange={event => {
+                          this.setState({ name: event.target.value });
+                        }}
+                        value={this.state.name}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        className="form-control form-control-user"
+                        type="text"
+                        id="ethAdd"
+                        aria-describedby="nameHelp"
+                        placeholder="ETH Address"
+                        name="ethAddress"
+                        onChange={event => {
+                          this.setState({ ethAddress: event.target.value });
+                        }}
+                        value={this.state.ethAddress}
+                      />
+                    </div>
+                    <hr />
                     <a
                       className="btn btn-primary btn-block text-white btn-google btn-user"
                       role="button"
+                      onClick={() => {
+                        review
+                          .createUser(this.state.address, this.state.name)
+                          .then(response => {
+                            review
+                              .getUser(this.state.address)
+                              .then(response =>
+                                runInAction(() => {
+                                  user.address = this.state.account;
+                                  user.details.name = response[0];
+                                  user.details.fileCount = response[1];
+                                  Router.replace("/dashboard");
+                                })
+                              )
+                              .catch(e =>
+                                console.log("*******************", e)
+                              );
+                          })
+                          .catch(e => {
+                            console.log(e);
+                          });
+                      }}
                     >
-                      <i className="fab fa-google" />&nbsp; Register with Google
+                      <i className="fab fa-google" />
+                      &nbsp; Register
                     </a>
                     <a
                       className="btn btn-primary btn-block text-white btn-facebook btn-user"
                       role="button"
                     >
-                      <i className="fab fa-facebook-f" />&nbsp; Register with
-                      Facebook
+                      <i className="fab fa-facebook-f" />
+                      &nbsp; Register with Facebook
                     </a>
                     <hr />
                   </form>

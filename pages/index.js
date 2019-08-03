65ebe620f00d65829f1a92ec681ebbfef5526cb0
feat7 <vinaykhobragade99@gmail.com>
2019-08-03 08:13:26 +0530

    ckpt: matic test files. Doesn't work.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,53 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+/* eslint-disable no-unused-vars */
+import Transfer from "../components/transfer";
+import Deposit from "../components/deposit";
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center'
-  },
-  text: {
-    alignItems: 'center',
-    fontSize: 24
-  }
-})
+function Home() {
+  return (
+    <div className="app">
+      <div>Welcome to Matic.js!</div>
+      <div className="parent">
+        <Transfer />
+        <Deposit />
+      </div>
+      <style jsx>{`
+        .app {
+          font-family: Helvetica, Arial, sans-serif;
+          font-size: 15px;
+          margin: 0;
+          text-align: center;
+        }
+        .parent {
+          transition: 100ms ease-in background;
+          display: flex;
+          flex: 1;
+          flex-direction: row;
+          justify-content: space-around;
+          align-items: center;
+        }
+      `}</style>
+      <style global jsx>{`
+        button {
+          padding: 15px 24px;
+          font-size: 16px;
+          line-height: 19px;
+          font-weight: 700;
+          border: none;
+          cursor: pointer;
+          border-radius: 5px;
+          background-color: #2b6def;
+          color: #fff;
+        }
+        button:hover {
+          background-color: #2460da;
+        }
+        .col {
+          flex: 1;
+          padding: 16px;
+        }
+      `}</style>
+    </div>
+  );
+}
 
-export default props => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Welcome to Next.js!</Text>
-  </View>
-)
+export default Home;

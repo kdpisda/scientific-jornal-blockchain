2e0784da07bf705f4b2d71a8573cc199c0bd7096
Shritesh <shritesh.sj@gmail.com>
2019-08-04 03:40:04 +0530

    latest contract

--- ethereum/app.js
+++ ethereum/app.js
@@ -2,7 +2,7 @@ var ethers = require("ethers");
 
 let web3Provider = ethers.getDefaultProvider("ropsten");
 
-let reviewContractAddress = "0x55248b60584b8dd5eb5d0e0fa3b8b9fbe78d19d6";
+let reviewContractAddress = "0x4433e01b900bc8e0e138bb6892a6b33b49ac4426";
 let storeContractAddress = "0xd71a648cf882ed954037136ee961f0db53e1689b";
 
 let privateKey =
